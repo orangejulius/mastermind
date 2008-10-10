ad9d76b7fafa029a5367dc7938dda0d291e2593b
Julian Simioni <julian.simioni@gmail.com>
2008-10-10 15:56:33 +0000

    basic multithreaded conversion

--- Agent.cpp
+++ Agent.cpp
@@ -10,54 +10,120 @@
 //
 //
 
+#include "Agent.h"
+
 #include <cstdlib>
 #include <pthread.h>
 #include <semaphore.h>
 
-#include "Agent.h"
+using namespace std;
 
 //global variables
 vector<unsigned> scores;
+unsigned total=0;
 
 //semaphores
 sem_t writeSem;//any thread wanting to write to a global variable must wait on this semaphore
 sem_t computing;
+sem_t threadLimit; //limit the max number of threads
+/*void playGame(void* gameNum)
+{
+	unsigned guesses=0;
+	int i=gameNum;
+	Environment e=Environment(50);
+	Agent a=Agent(&e);
+	char secret[4];
+	secret[0]=(i/(6*6*6))%6;
+	secret[1]=(i/(6*6))%6;
+	secret[2]=(i/(6))%6;
+	secret[3]=i%6;
+	State s(secret);
+	//cout<<secret<<endl;
+	e.setSecret(s);
+	a.play(guesses);
+	sem_wait(&writeSem);
+	while (scores.size()<guesses)
+	{
+		scores.push_back(0);
+	}
+	total+=guesses;
+	scores[guesses-1]++;
+	sem_post(&writeSem);
+}*/
+
+void *evenPlayGame(void* gameNum)
+{
+	for (int i=0;i<6*6*6*6;i+=2)
+	{
+		unsigned guesses=0;
+		Environment e=Environment(50);
+		Agent a=Agent(&e);
+		char secret[4];
+		secret[0]=(i/(6*6*6))%6;
+		secret[1]=(i/(6*6))%6;
+		secret[2]=(i/(6))%6;
+		secret[3]=i%6;
+		State s(secret);
+		//cout<<secret<<endl;
+		e.setSecret(s);
+		a.play(guesses);
+		sem_wait(&writeSem);
+		while (scores.size()<guesses)
+		{
+			scores.push_back(0);
+		}
+		total+=guesses;
+		scores[guesses-1]++;
+		sem_post(&writeSem);
+	}
+	sem_post(&computing);
+}
+
+void *oddPlayGame(void* gameNum)
+{
+	for (int i=1;i<6*6*6*6;i+=2)
+	{
+		unsigned guesses=0;
+		Environment e=Environment(50);
+		Agent a=Agent(&e);
+		char secret[4];
+		secret[0]=(i/(6*6*6))%6;
+		secret[1]=(i/(6*6))%6;
+		secret[2]=(i/(6))%6;
+		secret[3]=i%6;
+		State s(secret);
+		//cout<<secret<<endl;
+		e.setSecret(s);
+		a.play(guesses);
+		sem_wait(&writeSem);
+		while (scores.size()<guesses)
+		{
+			scores.push_back(0);
+		}
+		total+=guesses;
+		scores[guesses-1]++;
+		sem_post(&writeSem);
+	}
+	sem_post(&computing);
+}
 
 void stats()
 {
-    unsigned total=0;
-    for (int i=0;i<6*6*6*6;i++)
-    {
-        unsigned guesses=0;
-        if (i%10==0)
-        {
-            cout<<i<<endl;
-        }//*/
-        Environment e=Environment(50);
-        Agent a=Agent(&e);
-        char secret[4];
-        secret[0]=(i/(6*6*6))%6;
-        secret[1]=(i/(6*6))%6;
-        secret[2]=(i/(6))%6;
-        secret[3]=i%6;
-        State s(secret);
-        //cout<<secret<<endl;
-        e.setSecret(s);
-        a.play(guesses);
-        while (scores.size()<guesses)
-        {
-            scores.push_back(0);
-        }
-        total+=guesses;
-        scores[guesses-1]++;
-    }
+	pthread_t threads[2];
 
-    cout<<"moves, games solved in that # of moves"<<endl;
-    for (unsigned i=0;i<scores.size();i++)
-    {
-        cout<<i+1<<": "<<scores[i]<<endl;
-    }
-    cout<<"average: "<<total/1296.<<endl;
+	sem_init(&computing,0,-1);
+	sem_init(&writeSem,0,1);//limit number of threads accessing global variables to 1
+	int ignore=0;
+	pthread_create(&threads[0],NULL, evenPlayGame,(void*)ignore);
+	pthread_create(&threads[1],NULL, oddPlayGame,(void*)ignore);
+
+	sem_wait(&computing);
+	cout<<"moves, games solved in that # of moves"<<endl;
+	for (unsigned i=0;i<scores.size();i++)
+	{
+		cout<<i+1<<": "<<scores[i]<<endl;
+	}
+	cout<<"average: "<<total/1296.<<endl;
 }
 
 Agent::Agent(Environment* e)
