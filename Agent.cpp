9d8f82d1e6c5a00c05f2c0fea19a5056b312222b
Julian Simioni <julian.simioni@gmail.com>
2010-09-02 17:00:16 -0700

    Clarify Agent logic
    Tweak the Agent gameplay logic to remove two levels of
    indentation for greater readability.
    Also replace a for loop with a long initialization and no increment with
    a while loop.

--- Agent.cpp
+++ Agent.cpp
@@ -36,28 +36,28 @@ bool Agent::play(unsigned& guesses)
 		}
 
 		//ask the environment to score the guess
-		if (env->guess(guess, black, white)) {
-			//4 black pins means victory
-			if (black==4) {
-				return true;
-			} else {
-				int black2, white2;
-				//iterate through all possible solutions
-				//since Mastermind guess scores are symmetric, only combinations that score the same
-				//against the current guess as the current guess does against the solution are
-				//possible solutions
-				for (vector<State>::iterator i=possibleSolutions.begin();i!=possibleSolutions.end();) {
-					i->score(guess,black2,white2);
-					//remove the current combination from the list of possible solutions or move to the next combination
-					if ((black2!=black)||(white2!=white)) {
-						possibleSolutions.erase(i);
-					} else {
-						i++;
-					}
-				}
-			}
-		} else {
+		//false return value means no more guesses are allowed
+		if (env->guess(guess, black, white) == false) {
 			return false;
+		}
+		//4 black pins means victory
+		if (black == 4) {
+			return true;
+		}
+		int black2, white2;
+		//iterate through all possible solutions
+		//since Mastermind guess scores are symmetric, only combinations that score the same
+		//against the current guess as the current guess does against the solution are
+		//possible solutions
+		vector<State>::iterator i = possibleSolutions.begin();
+		while (i != possibleSolutions.end()) {
+			i->score(guess, black2, white2);
+			//remove the current combination from the list of possible solutions or move to the next combination
+			if (black2 != black || white2 != white) {
+				possibleSolutions.erase(i);
+			} else {
+				i++;
+			}
 		}
 	}
 }
