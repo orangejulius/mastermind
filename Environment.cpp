3d035e1607f0f0806375acb67ad8200a413969a7
Julian Simioni <julian.simioni@gmail.com>
2010-09-03 15:58:39 -0700

    Cleanup Environment.cpp
    Added proper spacing and an explicit constructor call for clarity.

--- Environment.cpp
+++ Environment.cpp
@@ -2,21 +2,21 @@
 
 Environment::Environment(unsigned max)
 {
-	secret="abcd";
-	guesses=0;
-	maxguesses=max;
+	secret = State("abcd");
+	guesses = 0;
+	maxguesses = max;
 }
 
 void Environment::setSecret(State newSecret)
 {
-	secret=newSecret;
+	secret = newSecret;
 }
 
 bool Environment::guess(State guess, int& black, int& white)
 {
-	if (guesses<maxguesses) {
+	if (guesses < maxguesses) {
 		guesses++;
-		secret.score(guess,black,white);
+		secret.score(guess, black, white);
 		return true;
 	} else {
 		return false;
